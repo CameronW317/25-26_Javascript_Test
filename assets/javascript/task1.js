5cebd3f0b07d8f3398a3db68a7419a5b76a5ab3e
15029 <cameronweiss021@gmail.com>
2025-12-15 08:40:55 -0500

    TABLE STYLE

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,14 @@
-/* Put code for the task below */
+function showButton() {
+    let button = document.getElementById("buttonTask1");
+
+    if (button.style.display === "none") {
+        button.style.display = "block";
+    }
+}
+function hidePicture(){
+    let image = document.getElementById("imageTask1");
+
+    if (image.style.display === "block") {
+        image.style.display = "none";
+    }
+}
--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,1 +1,12 @@
-/* Put code for the task below */
+function changeRed(){
+    let background = document.getElementById("table")
+    background.value.backgroundColor = "red"
+}
+function changeBlue(){
+    let background = document.getElementById("table")
+    background.value.backgroundColor = "blue"
+}
+function changeGreen(){
+    let background = document.getElementById("table")
+    background.value.backgroundColor = "green"
+}
